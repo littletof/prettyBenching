fb949986b6bb5230b02e51f5d995d148aa8f4ecb
littletof <szalay.kristof.sopron@gmail.com>
2020-06-11 19:51:06 +0200

    Add showcase changes

--- docs/showcase_create.ts
+++ docs/showcase_create.ts
@@ -1,7 +1,7 @@
-import { prettyBenchmarkProgress, prettyBenchmarkResult } from "./mod.ts";
+import { prettyBenchmarkProgress, prettyBenchmarkResult } from "../mod.ts";
 import {writeJsonSync, writeFileStrSync } from "https://deno.land/std@0.56.0/fs/mod.ts";
 
-import { bench, runBenchmarks, BenchmarkRunProgress, ProgressState} from "./deps.ts";
+import { bench, runBenchmarks, BenchmarkRunProgress, ProgressState} from "../deps.ts";
 
 
 
--- docs/showcase_read.ts
+++ docs/showcase_read.ts
@@ -1,7 +1,7 @@
-import { prettyBenchmarkProgress, prettyBenchmarkResult } from "./mod.ts";
+import { prettyBenchmarkProgress, prettyBenchmarkResult } from "../mod.ts";
 import {writeJsonSync, writeFileStrSync, readJsonSync } from "https://deno.land/std@0.56.0/fs/mod.ts";
 
-import { bench, runBenchmarks, BenchmarkRunProgress, ProgressState} from "./deps.ts";
+import { bench, runBenchmarks, BenchmarkRunProgress, ProgressState} from "../deps.ts";
 
 const progressData: any[] = readJsonSync("./docs/benchmark_progress_inputs.json") as any;
 const resultData = readJsonSync("./docs/benchmark_result_input.json");
