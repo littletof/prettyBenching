33f6cbe14f5d9094619dfde009ad82c295d53984
littletof <szalay.kristof.sopron@gmail.com>
2020-06-16 13:23:52 +0200

    Fmt

--- benchmarks/benchmark.ts
+++ benchmarks/benchmark.ts
@@ -22,7 +22,10 @@ const data = readJsonSync(
 const resultFn = prettyBenchmarkResult(
   {
     nocolor: false,
-    thresholds: { "multiple-runs": { green: 76, yellow: 82 }, "benchmark-start": { green: 2, yellow: 3 } },
+    thresholds: {
+      "multiple-runs": { green: 76, yellow: 82 },
+      "benchmark-start": { green: 2, yellow: 3 },
+    },
     indicators: [
       {
         benches: /multiple-runs/,
@@ -30,7 +33,7 @@ const resultFn = prettyBenchmarkResult(
         modFn: (str) => "🚀",
       },
     ],
-    parts: {threshold: true, extraMetrics: true, graph: true}
+    parts: { threshold: true, extraMetrics: true, graph: true },
   },
 );
 
