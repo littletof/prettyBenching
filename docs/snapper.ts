f70d301f97b4483ecf3b0230489277d2549c16fd
littletof <szalay.kristof.sopron@gmail.com>
2021-10-29 16:48:07 +0200

    Add all snap cases

--- docs/snapper.ts
+++ docs/snapper.ts
@@ -1,7 +1,7 @@
 import { snap } from 'https://deno.land/x/snapper@v0.0.5/mod.ts';
 import type { SnapParams } from 'https://deno.land/x/snapper@v0.0.5/mod.ts';
 import { join } from "https://deno.land/std@0.91.0/path/win32.ts";
-import { prettyBenchmarkProgress, prettyBenchmarkResult, prettyBenchmarkResultOptions } from "../mod.ts";
+import { deltaProgressRowExtra, deltaResultInfoCell, prettyBenchmarkHistory, prettyBenchmarkProgress, prettyBenchmarkProgressOptions, prettyBenchmarkResult, prettyBenchmarkResultOptions } from "../mod.ts";
 import { BenchmarkRunProgress, colors, ProgressState } from "../deps.ts";
 
 async function generateDocsImages() {
@@ -21,17 +21,27 @@ async function generateDocsImages() {
 
     const resultData = readJsonSync(join(pathBase, "benchmark_result_input.json"));
 
+    const postFix = '_snap.png';
 
     const thresholds = {"multiple-runs": { green: 76, yellow: 82 }};
     const indicators = [{benches: /multiple-runs/, color: colors.magenta,modFn: () => ({indicator: "🚀 ", visibleLength: 2})}];
+    const history = new prettyBenchmarkHistory({
+        history: [{date: new Date().toISOString(),
+            benchmarks: {
+                runs100ForIncrementX1e6: {measuredRunsAvgMs: 4.235, totalMs: 4235, runsCount: 1000},
+                for100ForIncrementX1e6: {measuredRunsAvgMs: 5.4405, totalMs: 5440.5, runsCount: 1000},
+                "multiple-runs": {measuredRunsAvgMs: 133.2134, totalMs: 13321.34, runsCount: 100}
+        }}]
+    });
 
     const optionsSet: {name: string, options: prettyBenchmarkResultOptions}[] = [ // TODO remove "snap" postfix
-        // {name: 'docs/imgs/prettyBenchingResult_example_threshold_snap.png', options: {thresholds}},
-        // {name: 'docs/imgs/prettyBenchingResult_example_indicator_snap.png', options: {indicators}},
-        // {name: 'docs/imgs/prettyBenchingResult_example_indicators_snap.png', options: {thresholds, indicators}},
-        // {name: 'docs/imgs/prettyBenchingResult_example_full_extra_snap.png', options: {thresholds, indicators: [{benches: /multiple-runs/, color: colors.blue,modFn: () => colors.bgYellow(colors.bold(colors.black("%")))}], parts: {extraMetrics: true,threshold: true,graph: true,graphBars: 10 }}},
-        // {name: 'docs/imgs/prettyBenchingResult_example_extrametrics_line_snap.png', options: {thresholds, parts: {extraMetrics: true}}},
-        // {name: 'docs/imgs/prettyBenchingResult_example_threshold_line_snap.png', options: {thresholds, parts: {threshold: true, graph: true}}},
+        {name: 'docs/imgs/prettyBenchingResult_example_threshold', options: {thresholds}},
+        {name: 'docs/imgs/prettyBenchingResult_example_indicator', options: {indicators}},
+        {name: 'docs/imgs/prettyBenchingResult_example_indicators', options: {thresholds, indicators}},
+        {name: 'docs/imgs/prettyBenchingResult_example_full_extra', options: {thresholds, indicators: [{benches: /multiple-runs/, color: colors.blue,modFn: () => colors.bgYellow(colors.bold(colors.black("%")))}], parts: {extraMetrics: true,threshold: true,graph: true,graphBars: 10 }}},
+        {name: 'docs/imgs/prettyBenchingResult_example_extrametrics_line', options: {thresholds, parts: {extraMetrics: true}}},
+        {name: 'docs/imgs/prettyBenchingResult_example_threshold_line', options: {thresholds, parts: {threshold: true, graph: true}}},
+        {name: 'docs/imgs/prettyBenchingHistory_result_card_delta', options: {infoCell: deltaResultInfoCell(history)}},
     ];
 
     const mr = {filtered: 0, results: [resultData.results[2]]};
@@ -45,19 +55,18 @@ async function generateDocsImages() {
 
     let result = "";
     prettyBenchmarkResult({outputFn: (res) => result += res})(resultData);
-    // snaps.push({content: result, imageSavePath: "docs/imgs/prettyBenchingResult_example_snap.png", viewport: {deviceScaleFactor: 1, width: 875, height: 595}});
-
+    snaps.push({content: result, imageSavePath: "docs/imgs/prettyBenchingResult_example", viewport: {deviceScaleFactor: 1, width: 780, height: 595}});
 
     /** Progress */
 
     const progressCases = [
-        {name: 'docs/imgs/prettyBenchingProgress_example_running_snap.png', state: buildProgressState([
+        {name: 'docs/imgs/prettyBenchingProgress_example_running', state: buildProgressState([
             fakeStart(10,0),
             fakeBenched('runs100ForIncrementX1e6', 100, 44.2054, 0.4421),
             fakeBenched('for100ForIncrementX1e6', 100, 45.1917, 0.4519),
             fakeProgress('for100ForIncrementx1e8', 100, 54)
         ]), options: {}},
-        {name: 'docs/imgs/prettyBenchingProgress_example_finished_snap.png', state: buildProgressState([
+        {name: 'docs/imgs/prettyBenchingProgress_example_finished', state: buildProgressState([
             fakeStart(8,0),
             fakeBenched('runs100ForIncrementX1e6', 100, 44.2054, 0.4421),
             fakeBenched('for100ForIncrementX1e6', 100, 45.1917, 0.4519),
@@ -67,9 +76,48 @@ async function generateDocsImages() {
             fakeBenched('forIncrementX1e9x2', 1, 12620.5453, 12620.5453),
             fakeBenched('single', 1, 0.0183, 0.0183),
             fakeBenched('multiple', 2, 0.0513, 0.0256),
-            fakeBenched('multiple', 100, 18.9176, 0.0189),
+            fakeBenched('custom', 100, 18.9176, 0.0189),
             fakeFinished()
         ]), options: {}},
+        {name: 'docs/imgs/prettyBenchingProgress_example_indicators', state: buildProgressState([
+            fakeBenched('runs100ForIncrementX1e6', 100, 44.2054, 0.4421),
+            fakeBenched('for100ForIncrementX1e6', 100, 45.1917, 0.4519),
+
+            fakeBenched('forIncrementX1e9', 1, 722.7738, 722.7738),
+            fakeBenched('single', 1, 0.0183, 0.0183),
+            fakeBenched('multiple', 2, 0.0513, 0.0256),
+            fakeBenched('custom', 100, 18.9176, 0.0189),
+        ]), options: {
+            indicators: [
+                { benches: /100/, modFn: () => colors.bgRed('#') },
+                { benches: /for/, modFn: () => colors.red('#') },
+                { benches: /multiple/, modFn: colors.cyan},
+                { benches: /custom/, modFn: () => colors.bgYellow(colors.black("%")) }
+            ]
+        } as prettyBenchmarkProgressOptions},
+        {name: 'docs/imgs/prettyBenchingProgress_example_threshold', state: buildProgressState([
+            fakeStart(5,3),
+            fakeBenched('runs100ForIncrementX1e6', 100, 68, 0.68),
+            fakeBenched('for100ForIncrementX1e6', 100, 108, 1.08),
+            fakeBenched('for100ForIncrementx1e8', 100, 9056, 90.56),
+            fakeBenched('forIncrementX1e9', 1, 848, 848),
+            fakeBenched('forIncrementX1e9x2', 1, 14904,14904),
+            fakeFinished()
+        ]), options: {
+            thresholds: {
+                "for100ForIncrementX1e6": {green: 0.85, yellow: 1},
+                "for100ForIncrementX1e8": {green: 84, yellow: 93},
+                "forIncrementX1e9": {green: 900, yellow: 800},
+                "forIncrementX1e9x2": {green: 15000, yellow: 18000},
+            }
+        } as prettyBenchmarkProgressOptions},
+        {name: 'docs/imgs/prettyBenchingHistory_progress_delta', state: buildProgressState([            
+            fakeBenched('runs100ForIncrementX1e6', 1000, 2831.9055, 2.8319),
+            fakeBenched('for100ForIncrementX1e6', 1000, 5687.3089, 5.6873),
+            fakeBenched('for100ForIncrementx1e8', 1000, 2754.4350, 2.7544),
+        ]), options: {
+            rowExtras: deltaProgressRowExtra(history)
+        } as prettyBenchmarkProgressOptions, viewport: {width: 1200}}
     ];
 
     snaps.push(...progressCases.map(c => {
@@ -79,11 +127,13 @@ async function generateDocsImages() {
         const states = buildProgressState(c.state);
         states.forEach(state => progressFnc(state));
 
-        return {content: result, imageSavePath: c.name,viewport:{width: 1000}} as SnapParams;
+        result = result.replace(/^\x1B\[1A\r/, ''); //replace first up1Cl causing empty line
+
+        return {content: result, imageSavePath: c.name,viewport:{width: 1000, ...c.viewport}} as SnapParams;
     }) as any)
 
 
-    await snap(snaps, {verbose: true});
+    await snap(snaps.map(s => ({...s, imageSavePath: s.imageSavePath + postFix})), {verbose: true, theme: {background: '#000'}});
 }
 
 function readJsonSync(path: string) {
